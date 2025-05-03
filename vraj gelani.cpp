35e64dd9ca00cbd64ab2b0266174af0cf61c1c27
vrajgelani <vrajgelani1@gmail.com>
2025-05-03 22:22:36 +0530

    Add files via upload

---
+++ vraj gelani.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#define PI 3.14159
+
+int main() 
+{
+    float radius, area;
+
+    
+    printf("Enter the radius of the circle: ");
+    scanf("%f", &radius);
+
+    area = PI * radius * radius;
+
+    printf("The area of the circle is: %.2f\n", area);
+
+}
