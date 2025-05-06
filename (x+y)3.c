22425016bc6edd6881ea4239ef74348b41ba2780
vrajgelani <vrajgelani1@gmail.com>
2025-05-06 16:51:20 +0530

    Add files via upload

---
+++ (x+y)3.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main ()
+{
+	float x,y,result;
+	
+	printf("enter the value of x:");
+	scanf("%f",&x);
+	
+	printf("enter the value of x:");
+	scanf("%f",&y);
+	
+	result=(x+y)*(x+y)*(x+y);
+	
+	printf("the result of (x+y)*(x+y)*(x+y)is:%f\n",result);
+	
+}
+/*
+input:x=12
+      y=12
+output:result=13824
+*/
