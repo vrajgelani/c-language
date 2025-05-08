4c7a804a6699269b84791c66a4ab18762541200c
vrajgelani <vrajgelani1@gmail.com>
2025-05-08 22:22:31 +0530

    Add files via upload

---
+++ (abc).c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main()
+{
+	int a,b,c;
+	
+	printf("enter the number a:");
+	scanf("%d",&a);
+	
+	printf("enter the number b:");
+	scanf("%d",&b);
+	
+	printf("enter the number c:");
+	scanf("%d",&c);
+	
+	if(a<b)
+	{
+	 if(a<c)
+	 printf("a is minimum number");
+	 else
+	printf("c is minimum number");
+	}
+	else
+	{if(b<c)
+	printf("b is minimum number");
+	else
+	printf("c is minimum number");
+	}
+}
